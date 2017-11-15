531353917aabf02474de054d044062e194cb5e0c
Steve <chengquan.wang@ele.me>
2017-11-15 14:22:48 +0800

    update the Example project

--- AlamofireCodable.podspec
+++ AlamofireCodable.podspec
@@ -21,11 +21,11 @@ Pod::Spec.new do |s|
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/wangchengqvan@gmail.com/AlamofireCodable'
+  s.homepage         = 'https://github.com/cnkcq/AlamofireCodable'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'wangchengqvan@gmail.com' => 'chengquan.wang@ele.me' }
-  s.source           = { :git => 'https://github.com/wangchengqvan@gmail.com/AlamofireCodable.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/cnkcq/AlamofireCodable.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
@@ -38,5 +38,5 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'Alamofire'
 end
--- Example/AlamofireCodable/ViewController.swift
+++ Example/AlamofireCodable/ViewController.swift
@@ -7,12 +7,74 @@
 //
 
 import UIKit
+import AlamofireCodable
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func objectRequest(_ sender: Any) {
+        let form = WeatherForm()
+        Alamofire.request(
+                form.url,
+                method: HTTPMethod.get,
+                parameters: form.parameters(),
+                encoding: form.encoding(),
+                headers: form.headers()
+            )
+            .responseObject(keyPath: "data",completionHandler: { (response: DataResponse<Weather>) in
+                switch response.result {
+                case .success(let object):
+                    debugPrint("🌹", object.location)
+                case .failure(let error):
+                    debugPrint("🌹", error.localizedDescription)
+                }
+            })
+    }
+    
+    @IBAction func arrayRequest(_ sender: Any) {
+        let form = WeatherForm()
+        Alamofire.request(
+                form.url,
+                method: HTTPMethod.get,
+                parameters: form.parameters(),
+                encoding: form.encoding(),
+                headers: form.headers()
+            )
+            .responseArray(keyPath: "data.three_day_forecast", completionHandler: { (response: DataResponse<[Forecast]>) in
+                switch response.result {
+                case .success(let array):
+                    debugPrint("🌹", array)
+                case .failure(let error):
+                    debugPrint("🌹", error.localizedDescription)
+                }
+            })
+//            .responseJSON { (response) in
+//                print("🌹", response.result.value.debugDescription)
+//        }
+    }
+    
+    @IBAction func nestedRequest(_ sender: Any) {
+        let form = WeatherForm()
+        Alamofire.request(
+            form.url,
+            method: HTTPMethod.get,
+            parameters: form.parameters(),
+            encoding: form.encoding(),
+            headers: form.headers()
+            )
+            .responseObject(completionHandler: { (response: DataResponse<RootModel>) in
+                switch response.result {
+                case .success(let root):
+                    debugPrint("🌹", root)
+                case .failure(let error):
+                    debugPrint("🌹", error.localizedDescription)
+                }
+            })
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +83,50 @@ class ViewController: UIViewController {
     }
 
 }
+
+struct RootModel: Codable {
+    
+    var data: Weather
+}
+
+
+struct Weather: Codable {
+    
+    var location: String
+    
+    var three_day_forecast: [Forecast]
+    
+}
+
+struct Forecast: Codable {
+    
+    var conditions: String
+    
+    var day: String
+    
+    var temperature: Int64
+}
+
+struct WeatherForm {
+    
+    var city = "shanghai"
+    
+    var method = Alamofire.HTTPMethod.get
+    
+    func parameters() -> [String: Any] {
+        return ["city": city]
+    }
+    
+    func encoding() -> ParameterEncoding {
+        return Alamofire.URLEncoding.default
+    }
+    
+    var url = "https://raw.githubusercontent.com/tristanhimmelman/AlamofireObjectMapper/"
+        + "2ee8f34d21e8febfdefb2b3a403f18a43818d70a/sample_keypath_json"
+    
+    public func headers() -> [String: String] {
+        return ["accessToken": "xxx"]
+    }
+}
+
 
